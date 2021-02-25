06584e5c0318f6594e437fb860d07d4151a0ab1b
Ramon Lence <rlence86@gmail.com>
2021-02-25 15:34:50 +0100

    added unit tests and snapshot for application component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,75 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+@Component({
+  template: '<div id="dummy-element">Dummy element</div>'
+})
+class DummyComponent {
+}
+
+@Component({
+  selector: 'app-header',
+  template: '<div id="header-element">Header element</div>'
+})
+class DummyHeader {
+}
+
+@Component({
+  selector: 'app-footer',
+  template: '<div id="footer-element">Footer element</div>'
+})
+class DummyFooter {
+}
+
 describe('AppComponent', () => {
+  let router: Router;
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([
+          { path: '', component: DummyComponent }
+        ])
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        DummyComponent,
+        DummyHeader,
+        DummyFooter
       ],
     }).compileComponents();
+    router = TestBed.inject(Router);
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jokill-web'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('jokill-web');
+  it(`should have a header and a footer'`, () => {
+    const childHeadElement = fixture.debugElement.query(By.css("#header-element"));
+    expect(childHeadElement.nativeElement.textContent).toBe('Header element');
+    const childFooterlement = fixture.debugElement.query(By.css("#footer-element"));
+    expect(childFooterlement.nativeElement.textContent).toBe('Footer element');
   });
+
+  it(`should have routing working`, fakeAsync(() => {
+    router.navigateByUrl('');
+    tick();
+    const childRouter = fixture.debugElement.query(By.css("#dummy-element"));
+    expect(childRouter.nativeElement.textContent).toBe('Dummy element');
+  }));
+
+  it(`Renders correctly`, fakeAsync(() => {
+    router.navigateByUrl('');
+    tick();
+    expect(fixture).toMatchSnapshot();
+  }));
 
 });
